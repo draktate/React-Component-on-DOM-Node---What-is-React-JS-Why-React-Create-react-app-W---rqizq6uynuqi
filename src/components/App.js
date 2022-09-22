882fe05a9de52b7e6ee53ad27ff327bcf0bac068
draktate <draktate@gmail.com>
2022-09-22 19:37:57 +0530

    Initial load

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,22 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useEffect, useState } from "react";
+import '../styles/App.css';
+
+const App = () => {
+
+  let [count, setCount] = useState(0)
+
+  return (
+    <div >
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+
+    </div>
+  );
+}
+
+
+export default App;
+
+
+
+
 
